ffdf63e249b0a99f3e3ffa89f4f3c6fe276870ae
Frederick <holmes.frederick@gmail.com>
2022-06-27 22:11:43 -0500

    first commit

---
+++ src/Language.java
@@ -0,0 +1,15 @@
+public class Language {
+
+    public static void main(String[] args) {
+        int number = 4;
+        System.out.println(number);
+    }
+}
+
+
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/fredtroquet/FirstProgram.git
+        git push -u origin main
