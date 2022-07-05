e39c2b2f88a19bbd3568982f3b5938af098ae9d9
Frederick <holmes.frederick@gmail.com>
2022-07-04 22:16:38 -0500

    Initial commit

---
+++ src/Try1.java
@@ -0,0 +1,24 @@
+/*
+
+class level comments
+ */
+public class Try1 {
+
+    /*
+        jjkldjfkjasdlk
+        sjaljfldjsljfasdk
+        jlajdflkj
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!" + (2 + 3));
+        System.out.println("Hello, World! + (2+3)");
+        System.out.println(3 * 4);
+        System.out.println(3 * 4.5);
+        System.out.println(args[0]);
+
+        int num = 10;
+        num = num + 1;
+        // lsjaljflkdsjlkfjlsdklfjasldjflkds
+        System.out.println("" + num); // lsjaljflkdsjlkfjlsdklfjasldjflkds
+    }
+}
